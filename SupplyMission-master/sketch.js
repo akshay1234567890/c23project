7f2a841283d24dcc249c4e778754129307dc687e
akshay1234567890 <64688767+akshay1234567890@users.noreply.github.com>
2020-11-26 18:04:52 +0530

    Add files via upload

---
+++ SupplyMission-master/sketch.js
@@ -0,0 +1,85 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+															
+function preload()			
+{
+	helicopterIMG=loadImage("helicopter.png");
+	packageIMG=loadImage("package.png");
+}
+
+function setup() {						
+	createCanvas(800, 700);
+	rectMode(CENTER);									
+	
+
+	packageSprite=createSprite(0, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2	
+
+	helicopterSprite=createSprite(0, 200, 10,10);		
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+	helicopterSprite.velocityX = 3
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create(); 
+	world = engine.world;
+
+
+	rightBody = Bodies.rectangle(380,610,20,100 , {isStatic : true});
+	World.add(world,rightBody);
+
+	bottomBody = Bodies.rectangle(490,650,210,20, {isStatic : true});
+	World.add(world,bottomBody);
+
+	leftBody = Bodies.rectangle(600,610,20,100  , {isStatic : true});
+
+	packageBody = Bodies.rectangle(width/2 ,200,35,20,{restitution:0.75, isStatic:true});
+	World.add(world,	 packageBody);
+	
+
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine); 		
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);				
+
+  packageSprite.x= packageBody.position.x 
+  packageSprite.x= helicopterSprite.x
+  packageSprite.y= packageBody.position.y 
+  fill("red")
+  rect(rightBody.position.x,rightBody.position.y,20,100)
+
+  fill("red")
+  rect(bottomBody.position.x,bottomBody.position.y,200,20)
+  
+  rect(leftBody.position.x,leftBody.position.y,20,100)
+  drawSprites();		
+ 
+}x
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the doc	ument and understand how to make the package body fall only on
+	Matter.Body.setStatic(packageBody,false);
+	helicopterSprite.velocityX = 0;
+  }
+}
+
+
